cf290398e05936d72e1a219408bfaf774c15ae71
Moon <moon@Moonui-MacBook-Pro.local>
2018-08-05 15:06:43 +0900

    delete hash history

--- src/client/Root.js
+++ src/client/Root.js
@@ -2,12 +2,10 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import App from '../App'
 
-import createHashHistory from 'history/createHashHistory';
-const hashHistory = createHashHistory({ basename: process.env.PUBLIC_URL });
     
 
 const Root = () => (
-    <BrowserRouter history={hashHistory} basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
         <App />
     </BrowserRouter>
 );
